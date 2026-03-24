7033cbddec5b2a387bb2669bce9db6dfc237b1b7
Thành Vinh <>
2026-03-24 11:33:02 +0700

    Change CV file and update something data

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -64,7 +64,7 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, {`I'm`} Vinh.</span> {`I'm`} a{" "}
-        <span className="font-bold">Front-end & ios developer</span> with{" "}
+        <span className="font-bold">Frontend & Mobile developer</span> with{" "}
         <span className="font-bold">about 3 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React framework</span>.
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -40,7 +40,7 @@ export const experiencesData = [
     location: "Building F, Software Park Quang Trung, Tan Chanh Hiep ward, 12 district, HCM City",
     description: "I graduated after about 2 years of studying. I chosen core is front-end developer.",
     icon: React.createElement(LuGraduationCap),
-    date: "08/2017 - 06/2020",
+    date: "Aug 2017 - Jun 2020",
   },
   {
     title: "Front-End Developer",
@@ -52,31 +52,31 @@ export const experiencesData = [
   {
     title: "IOS Developer",
     location: "303, Pham Van Dong street, Ward 1, District Go Vap, HCM City",
-    description: "After this i as a ios developer for about more than 1 year in right this company.",
+    description: "After this is as a ios developer for about more than 1 year in right this company.",
     icon: React.createElement(FaSwift),
     date: "2023 - 2024",
   },
   {
     title: "Front-End Developer",
     location: "92, Yen The street, Ward 2, District Tan Binh, HCM City",
-    description: " To meet the job requirements, I learned about the react and react native use typescript. And worked with react and react native use typescript",
+    description: "To meet the job requirements, I learned about the react and react native use typescript. And worked with react and react native use typescript",
     icon: React.createElement(FaReact),
-    date: "2024 - 2025",
+    date: "2024 - Dec 2025",
   },
   {
     title: "Front-End Developer",
-    location: "42, Luong Ngoc Quyen street, Ward 5, District Go Vap, HCM City",
+    location: "1007, Quang Trung street, Ward 10, District Go Vap, HCM City",
     description:
       "I'm now a front-end developer working as a freelancer. My stack includes React, React Native, Next.js, TypeScript, Tailwind and Swift. I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
-    date: "2025 - present",
+    date: "2026 - Present",
   },
 ] as const;
 
 export const projectsData = [
   {
     title: "Techres Seemt - Operations Management",
-    description: "I worked as a ios developer on this startup project for about 2 years. This application can manage all the business processes of the restaurant, including human resource management.",
+    description: "I worked as a ios developer on this startup project for about 1 years. This application can manage all the business processes of the restaurant, including human resource management.",
     tags: ["Swift 5", "Socket.io", "Firebase", "Push Notification", "Rest API", "Realm"],
     imageUrl: seemtApp,
   },
@@ -90,8 +90,8 @@ export const projectsData = [
   {
     title: "TTL Dashboard",
     description:
-      "I worked as a front-end developer for about more than 1 year. Users can management products, orders, customer, affiliate and partner to website.",
-    tags: ["React", "React Redux", "Tailwind", "Easy Email Editor", "Mantine Datatable", "Rest API"],
+      "I worked as a front-end developer for about 2 year. Users can management products, orders, customer, affiliate and partner to website.",
+    tags: ["React", "Redux Toolkit", "Tailwind", "Easy Email Editor", "Mantine Datatable", "Rest API"],
     imageUrl: ttlDashboard,
   }
 ] as const;
@@ -99,13 +99,10 @@ export const projectsData = [
 export const skillsData = [
   "HTML",
   "CSS",
-  "JavaScript",
   "TypeScript",
   "React",
   "React Native",
   "Next.js",
-  "Nest.js",
-  "MongoDB",
   "Resend Email",
   "Easy Email",
   "Swift",
@@ -116,6 +113,8 @@ export const skillsData = [
   "Git",
   "Tailwind",
   "Bootstrap",
-  "React Redux",
+  "Mantine UI",
+  "Material UI",
+  "Redux Toolkit",
   "Framer Motion",
 ] as const;
